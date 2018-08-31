93f42a834aaa240da26a1e7ec2b2cf1b386eca39
Nitin Vinayak Agrawal <f20170415@gmail.com>
2018-08-31 12:40:33 +0530

    added

---
+++ SimpleBT/SimpleBT.ino
@@ -0,0 +1,14 @@
+int rx=8;
+int tx=7;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(rx,INPUT);
+  pinMode(tx,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
